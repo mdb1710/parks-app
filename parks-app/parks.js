1db92c1c4d67252ad2ac1f5f7e94c7031f2c275b
Michael Bonner <michaelb@thinkful.com>
2019-01-25 15:57:27 -0500

    moved files

---
+++ parks-app/parks.js
@@ -0,0 +1,7 @@
+// AAiy1c1UiI3L0f6vLtwCVz0U37nstlyd7aJq9XQl
+
+//https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=AAiy1c1UiI3L0f6vLtwCVz0U37nstlyd7aJq9XQl
+
+
+const APIKEY = 'AAiy1c1UiI3L0f6vLtwCVz0U37nstlyd7aJq9XQl';
+
