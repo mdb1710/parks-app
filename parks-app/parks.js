7ba82f1217086eb24c27b20f8bf894a0d5c7e9cd
owencyr <cyroc@dukes.jmu.edu>
2019-01-25 19:33:32 -0500

    parks lookup implemented

--- parks-app/parks.js
+++ parks-app/parks.js
@@ -10,21 +10,32 @@ const APIKEY = 'AAiy1c1UiI3L0f6vLtwCVz0U37nstlyd7aJq9XQl';
 
 const searchURL = 'https://developer.nps.gov/api/v1/parks';
 
-function apiCall (stateCode, q, maxResults=10) {
+function apiCall (stateCode, q, limit=10) {
   const params = {
     key: APIKEY,
     q,
     stateCode,
-    maxResults
+    limit
   };
 
   let pKeys = Object.keys(params);
 
-  let parksAPIURL = `${searchURL}?api_key=${params.key}&${pKeys[1]}=${params.q}&${pKeys[2]}=${params.stateCode}&${pKeys[3]}=${params.maxResults}`;
-console.log(parksAPIURL);
+  let parksAPIURL = `${searchURL}?api_key=${params.key}&${pKeys[1]}=${params.q}&${pKeys[2]}=${params.stateCode}&${pKeys[3]}=${params.limit}`;
+  //   console.log(parksAPIURL);
   fetch(parksAPIURL)
-    .then(response => response.json())
-    .then(responseJSON => displayResults(responseJSON))
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error (response.statusText);
+    })
+    .then(responseJSON => {
+      if (responseJSON.data.length === 0) {
+        $('.results').html('No parks in this city/state');
+      }
+      else {
+        displayResults(responseJSON);
+      }})
     .catch((err) => console.log(err));
 
   console.log('fetch ran');
@@ -32,14 +43,33 @@ console.log(parksAPIURL);
 
 function displayResults (array){
   console.log(array);
+  
+  let html = generateString(array);
+  $('.results').html(html);  
 }
 
 
-function generateHTML() {
+function generateString(array) {
+  return array.data.map((item, index) => generateHTML(item)).join(' ');
 
+// FULL NAME, DESCRIPTION, WEBSITE URL
+  //FULL NAME
+//   array.data[i].fullName
+  //DESC
+//   array.data[i].description
+  // WEBSITE URL
+//   array.data[i].url
 }
 
-
+function generateHTML (item) {
+  return `
+    <ul>
+        <li>${item.fullName}</li>
+        <li>${item.description}</li>
+        <li>${item.url}</li>
+    </ul>
+    `;
+}
 
 
 function handleSubmit (){
@@ -48,10 +78,10 @@ function handleSubmit (){
     console.log('submit detected');
     const stateCode = $('#search-state').val();
     const query = $('#search-city').val();
-    const maxResults = $('#search-result').val();
-    apiCall(stateCode, query, maxResults);
+    const limit = $('#search-result').val();
+    apiCall(stateCode, query, limit);
   });
-console.log('ready to handle submits');
+  console.log('ready to handle submits');
 }
 
 function handleParksSearch () {
